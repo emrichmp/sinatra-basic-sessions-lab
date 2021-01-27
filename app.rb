3befbbfc6fecd833aed13d2fcd44493df87e9e1e
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-01-26 23:33:49 -0500

    commit tree 00228a042caee77cb8593ed6dba5970270b31f96

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session_secret, "Penis"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    get '/checkout' do
+        @sessions = sessions
+        item = params["item"]
+        @sessions[:item] = item
+    end
 end
